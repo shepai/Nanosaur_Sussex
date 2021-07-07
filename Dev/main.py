c9dbc4b5cd584a1958f7094c193d86febe3efdbb
shepai <dextershepherd@hotmail.co.uk>
2021-07-07 13:03:19 +0100

    horz flow updated

--- Dev/main.py
+++ Dev/main.py
@@ -139,11 +139,9 @@ def getImage(): #return the image
 def getOpticalFlow(im1,im2): #get the optical flow from previous, current 
     flow = cv2.calcOpticalFlowFarneback(im1,im2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-    return rgb
+    horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX) #horizontal flow
+    horz = horz.astype('uint8')
+    return horz
 ###########
 #Define needed variables
 ###########
