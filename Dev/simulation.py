a46d037ded37cd7e564a4e2bec5f7f2c0f4f7b6a
shepai <dextershepherd@hotmail.co.uk>
2021-07-28 15:09:38 +0100

    more comments

--- Dev/simulation.py
+++ Dev/simulation.py
@@ -25,11 +25,11 @@ import torch
 plt.ylim((-50,50))
 plt.xlim((-50,50))
 
-def getBump(a,points):
+def getBump(a,points): #get whether it has met an obstacle
     if [a.x,a.y] in points:
         return True
     return False
-def fitness(numBumps,timeGiven):
+def fitness(numBumps,timeGiven): #gather the fitness based on time and bumps
     #print((timeGiven-numBumps)/20)
     return (timeGiven-numBumps)/20
 
@@ -40,7 +40,7 @@ def mutation(gene, mean=0, std=0.1):
     gene[gene < -4] = -4
     return gene
 
-def crossover(loser, winner, p_crossover=0.5):
+def crossover(loser, winner, p_crossover=0.5): #provide a crossover function
     for i,gene in enumerate(winner):
       if random.random() <= p_crossover:
         loser[i] = winner[i]
@@ -84,7 +84,7 @@ class Agent:
         
         return a
     
-class agent:
+class agent: #agent control in the simulation
     def __init__(self):
         self.x=0
         self.y=0
@@ -93,7 +93,7 @@ class agent:
         self.vector=[x,y]
         self.x+=x
         self.y+=y
-    def get_image(self,points,r=8):
+    def get_image(self,points,r=8): #radar simulation
         #get array facing the vector
         #(x-self.x)^2+ (y-self.y)^2 = 25
         arr=[]
@@ -122,7 +122,7 @@ class agent:
 
 
 
-def show_points(points,agent):
+def show_points(points,agent): #show points if you wish to visualize
     plt.cla()
     for p in points:
         plt.scatter(p[0],p[1],c="y")
@@ -158,7 +158,7 @@ fitness_index=[0 for i in range(pop_size)]
 
 TIME=20
 
-for gen in range(Generations):
+for gen in range(Generations): #microbial algorithm
     a=agent()
     #perform Reinforcement learning 
     current=a.get_image(points)
@@ -177,9 +177,9 @@ for gen in range(Generations):
     ag.set_genes(g1) #set the genes
     while currentT-t<TIME and getBump(a,points)==False: #give 20 seconds for trial
         currentT=time.time()
-        current=a.get_image(points)
-        action=ag.get_action(current)
-        a.set_vector(action[0],action[1])
+        current=a.get_image(points) #get radar reading
+        action=ag.get_action(current) #get motor control command
+        a.set_vector(action[0],action[1]) #set a vector for movement
         show_points(points,a)
         time.sleep(0.1)
         prvs = copy.deepcopy(current)
